12f64eb88f0b5bd9150ea4f7f816cbd0b439ec42
Niraj <your@gmail.com>
2026-06-23 17:48:44 +0530

    first commit

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify, render_template_string
 from google import genai
 
+from flask import Flask
+
 app = Flask(__name__)
 import os
 client = genai.Client(api_key=os.environ.get("GEMINI_API_KEY"))
@@ -499,6 +501,8 @@ def generate():
         print(f"ERROR: {e}")
         return jsonify({"captions": str(e)})
 
-if __name__ == "__main__":
-    app.run(debug=True)
+import os
 
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
